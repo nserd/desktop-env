f3616d41db9d0d295c39de6d93e1b30a3cc76889
Nikita Serdyukov <serdiukovn@gmail.com>
2025-05-14 20:52:33 +0300

    add alias

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e
 
-sudo dnf install -y git && \
-git clone https://github.com/nserd/desktop-env && \
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-python get-pip.py && \
-rm get-pip.py && \
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
 pip install ansible
+sudo dnf install -y git
+git clone https://github.com/nserd/desktop-env
+
+echo -e "\nTo start the installation, run the command:"
+echo -e "\e[0;1mcd desktop-env && ansible-playbook <playbook_name>.yml -D\e[0m"
